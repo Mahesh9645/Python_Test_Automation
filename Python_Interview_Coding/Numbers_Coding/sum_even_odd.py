76357191bbc44e6d2b36c0bb5de201c4fb7c8660
Mahesh <vmahesh9645@gmail.com>
2025-03-21 07:32:12 +0530

    new updates

--- Python_Interview_Coding/Numbers_Coding/sum_even_odd.py
+++ Python_Interview_Coding/Numbers_Coding/sum_even_odd.py
@@ -23,3 +23,24 @@ odd_sum = sum([i for i in list2 if i % 2 != 0])
 # Print the sums of even and odd numbers
 print(f'Sum of even numbers: {even_sum}')
 print(f'Sum of odd numbers: {odd_sum}')
+
+##################################################################################################
+# Given string of objects  find the sum of all possiable given digits      company name :zoho
+num = 1546827
+even_sum = 0
+odd_sum = 0
+odd_list = []
+
+for i in str(num):  # Convert the number to a string to loop through each digit
+    if int(i) % 2 != 0:  # If the digit is odd
+        odd_list.append(int(i))  # Add the odd digit to the odd_list
+        odd_sum += int(i)  # Add the odd digit to the odd_sum
+    else:  # If the digit is even
+        even_sum += int(i)  # Add the even digit to the even_sum
+
+
+print(f'Even sum: {even_sum}')
+print(f'Odd sum: {odd_sum}')
+print(f'Odd digits: {odd_list}')
+
+print(odd_list)
